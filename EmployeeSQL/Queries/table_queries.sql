0a1c423c0909b0d0723e9a22e89230832f2120af
Violetta Haveman <120049344+vihaveman@users.noreply.github.com>
2023-04-21 20:07:33 -0400

    Create table_queries.sql

---
+++ EmployeeSQL/Queries/table_queries.sql
@@ -0,0 +1,20 @@
+SELECT * From departments
+	limit 10; 
+
+SELECT * From dept_emp
+ limit 10; 
+
+
+SELECT * From dept_manager
+	limit 10; 
+
+
+SELECT * From employees
+	limit 10; 
+	
+
+SELECT * From salaries
+	limit 10; 
+
+SELECT * From titles
+	limit 10; 
