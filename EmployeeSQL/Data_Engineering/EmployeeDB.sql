4ef6eb37fef69ad0d862beb143f96e710b4a7cbc
Violetta Haveman <120049344+vihaveman@users.noreply.github.com>
2023-04-24 18:33:38 -0400

    Update EmployeeDB.sql

--- EmployeeSQL/Data_Engineering/EmployeeDB.sql
+++ EmployeeSQL/Data_Engineering/EmployeeDB.sql
@@ -14,7 +14,7 @@ CREATE TABLE Titles (
 
 CREATE TABLE Employees (
     emp_no INT   NOT NULL,
-    emp_title_id VARCHAR(4)   NOT NULL,
+    emp_title_id VARCHAR(5)   NOT NULL,
     birth_date DATE   NOT NULL,
     first_name VARCHAR(30)   NOT NULL,
     last_name VARCHAR(30)   NOT NULL,
